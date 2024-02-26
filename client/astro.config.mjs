74b35a83392ca160000cbf810e0ad419f3cf10f2
lippiece <github@lippiece.anonaddy.me>
2024-02-26 20:49:44 +0300

    feat(landing): lightbox, #flamenco

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs({ entrypoint: "./src/alpine" })],
+  integrations: [alpinejs({ entrypoint: "./alpine" })],
 })
-
