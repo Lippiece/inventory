003f7b235a87ad65c3542dff52455c098797b7cd
lippiece <github@lippiece.anonaddy.me>
2024-03-29 21:19:36 +0200

    feat: begin rewrite with new design

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,8 +1,14 @@
 import alpinejs from "@astrojs/alpinejs"
 import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   base: "/inventory",
-  integrations: [alpinejs({ entrypoint: "./alpine" })],
+  integrations: [
+    alpinejs({ entrypoint: "./alpine" }),
+    tailwind({
+      nesting: true,
+    }),
+  ],
 })
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*"],
+  plugins: [],
+
   theme: {
     extend: {},
   },
-  plugins: [],
 }
-
