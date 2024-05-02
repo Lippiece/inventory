04488ddf53aa545250411443e9609c2312d82252
lippiece <github@lippiece.anonaddy.me>
2024-05-02 20:50:44 +0200

    feat(app):implement serving astro statics

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import "./setup"
 
 import { Hono } from "hono"
-import { basicAuth } from "hono/basic-auth"
 import { logger } from "hono/logger"
+import { serveStatic } from "hono/bun"
+// import { serveStatic } from "@hono/node-server/serve-static"
 
 import errorHandler from "./functions/errorHandler"
 import api from "./routes/api"
@@ -11,18 +12,9 @@ const app = new Hono()
 
 // Middleware
 app.use(logger())
-app.use(
-  "/auth/*",
-  basicAuth({
-    password: "acoolproject",
-    username: "hono",
-  }),
-)
 
 // Routes
-app
-  .get("/", context => context.redirect("/api"))
-  .get("/error", context => context.text(new Error("An error")))
+app.use("/*", serveStatic({ root: "dist/" }))
 
 app.route("/api", api)
 
